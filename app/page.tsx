9e69391919a62adbbe7c6094c27cbd3b747af0e7
hdamouh-jpg <h.damouh@gmail.com>
2026-01-12 10:39:59 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Gest_Farms</h1>
+      <p>Gestion des fermes avicoles (Poulet & Dinde)</p>
+
+      <p>Application en cours de création 🚧</p>
+    </main>
+  );
+}
